04e818fe1fa016bcd5be66ff412a6e9c99925b14
sakthisvs <66023515+sakthisvs@users.noreply.github.com>
2025-08-16 23:33:24 +0530

    Add files via upload
    Employee details - Home Assignments

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,32 @@
+package week.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) 
+	{
+		String fullName = "John Doe";
+		 short age=30;
+		 double salary = 55000.75;
+		 char gender ='M';
+		 boolean acceptTerms =true;
+		 float rating= 4.5f;
+		 long mobileNumber = 9840912402l;
+		 int pincode = 56001;
+		 byte luckyNumber = 7;
+		 
+		 System.out.println("Employee Full name is :"+fullName );
+		 System.out.println("Age is:" +age );
+		 System.out.println("Monthly salary is:" +salary );
+		 System.out.println("Gender:"+gender);
+		 System.out.println("Accept Terms:"+acceptTerms);
+		 System.out.println("Rating is:"+rating);
+		 System.out.println("Mobile Number is:"+mobileNumber);
+		 System.out.println("Pincode is:"+pincode);
+		 System.out.println("Lucky number is:"+luckyNumber);
+		
+		 
+		
+
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,12 @@
+package week.day1;
+
+public class HelloTestLeaf {
+	
+	public static void main(String[] args) {
+		System.out.println(" Welcome to Test leaf" );
+		
+                       byte noOfLearners =60;
+                       System.out.println(noOfLearners);   
+	}
+
+}
